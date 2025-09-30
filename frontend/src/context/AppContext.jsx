2e9d373d6a361a66c10e636dc0f622a078d0ccdf
sajalchandhoke <chandhokesajal3@gmail.com>
2025-09-30 23:49:42 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -37,7 +37,7 @@ const AppContextProvider = (props) => {
 
         try {
 
-            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+            const { data } = await axios.get(${backendUrl} + '/api/user/get-profile', { headers: { token } })
 
             if (data.success) {
                 setUserData(data.userData)
