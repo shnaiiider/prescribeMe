dc502f17758f77372ca3db31cf55560b927e1c1c
sajalchandhoke <chandhokesajal3@gmail.com>
2025-10-02 20:11:43 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = "https://prescribeme-backend-ojwy.onrender.com"
+    const backendUrl = "https://prescripto-full-stack-13ze.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
